e4f7431dfddd1dcac98a0b00fe1515dce25e1cb9
hsz_global <eric0078@163.com>
2017-08-27 19:46:30 +0800

    乱码修改

--- src/com/atguigu/git/test/GitTest.java
+++ src/com/atguigu/git/test/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.out.println("hello 0427");
-		System.out.println("hello github ·şÎñÆ÷");
+		System.out.println("hello github æœåŠ¡å™¨");
 	}
 }
