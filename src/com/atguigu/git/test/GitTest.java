513b8bf4e4aa5377800e65e55a2dcab2ff55a45f
hsz_global <eric0078@163.com>
2017-08-27 21:13:16 +0800

    aaasddasd

--- src/com/atguigu/git/test/GitTest.java
+++ src/com/atguigu/git/test/GitTest.java
@@ -7,6 +7,6 @@ public class GitTest {
 		System.out.println("hello github 服务器");
 		System.out.println("hello 服务器回应1");
 		System.out.println("upeate p1 edit");
-		System.out.println("haha1234;
+		System.out.println("haha1234");
 	}
 }
