729544ccb464e439afa34d3bc05318ed59f75fc1
Zengo <31237212+omigard@users.noreply.github.com>
2017-08-27 19:47:56 +0800

    server update
    server update

--- src/com/atguigu/git/test/GitTest.java
+++ src/com/atguigu/git/test/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("hello 0427");
 		System.out.println("hello github 服务器");
+		System.out.println("hello 服务器回应");
 	}
 }
