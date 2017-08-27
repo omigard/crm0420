2c255f8371f52e24b2b48cbba33c56a1116a6c94
Zengo <31237212+omigard@users.noreply.github.com>
2017-08-27 19:54:21 +0800

    1
    1

--- src/com/atguigu/git/test/GitTest.java
+++ src/com/atguigu/git/test/GitTest.java
@@ -5,6 +5,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("hello 0427");
 		System.out.println("hello github 服务器");
-		System.out.println("hello 服务器回应");
+		System.out.println("hello 服务器回应1");
 	}
 }
